789097b64e3fa6063dcb238836ad04cb3fa333ab
Gloria Moreno <119625306+gloria2105@users.noreply.github.com>
2023-08-31 12:54:02 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-window.onload = function() {
-            alert('"Bienvenidos a Pet Pals, aqui encontraras todo para tu mascota"'."Adelante")
-};
+
 const botonesAgregar = document.querySelectorAll('.agregar-carrito');
 const notificacionAgregado = document.getElementById('notificacion-agregado');
 const listaCarrito = document.getElementById('lista-carrito');
